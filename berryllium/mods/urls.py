fad6e5e97ce4ac2423103dd5ccbb9df55234dade
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:02:15 -0700

    Configure url for upload_step1

--- berryllium/mods/urls.py
+++ berryllium/mods/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from . import views
+from .views import upload
 
-urlpatterns = [path("mods/upload/", views.upload_mod, name="upload_mod")]
+urlpatterns = [
+    path("mods/upload/", upload.upload_mod, name="upload_mod"),
+    path("mods/upload/s1", upload.upload_step1, name="upload_step1")
+]
+    
