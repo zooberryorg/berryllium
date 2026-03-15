f0f677f60ec1b7bbbc402c8d6a266e1b1812582c
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:13:35 -0700

    Reformat all files

--- berryllium/mods/urls.py
+++ berryllium/mods/urls.py
@@ -3,6 +3,5 @@ from .views import upload
 
 urlpatterns = [
     path("mods/upload/", upload.upload_mod, name="upload_mod"),
-    path("mods/upload/s1", upload.upload_step1, name="upload_step1")
+    path("mods/upload/s1", upload.upload_step1, name="upload_step1"),
 ]
-    
--- berryllium/mods/views/upload.py
+++ berryllium/mods/views/upload.py
@@ -9,8 +9,9 @@ def upload_mod(request):
     """
     return render(request, "mods/upload/base.html")
 
+
 def upload_step1(request):
     """
     Step 1 of the upload form.
     """
-    return render(request, 'mods/upload/step/1.html')
+    return render(request, "mods/upload/step/1.html")
