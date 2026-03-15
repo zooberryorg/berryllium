c6f89e8fce9a07855a4efe144fc38a9d0f50bd5d
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 01:40:57 -0700

    Create and configure landings application

--- berryllium/settings.py
+++ berryllium/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'berryllium.shared'
+    'berryllium.shared',
+    'berryllium.landings'
 ]
 
 MIDDLEWARE = [
--- berryllium/urls.py
+++ berryllium/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shared/', include('berryllium.shared.urls'))
+    path('shared/', include('berryllium.shared.urls')),
+    path('', include('berryllium.landings.urls'))
 ]
