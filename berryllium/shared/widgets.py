7489eabee5e42858562c266a26b6cbbaa0f54eb6
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:25:28 -0700

    Port shared/widgets.py from old codebase

---
+++ berryllium/shared/widgets.py
@@ -0,0 +1,5 @@
+from django.forms import CheckboxSelectMultiple
+
+class PillCheckboxSelectMultiple(CheckboxSelectMultiple):
+    template_name = "core/widgets/pill_checkbox_select.html"
+    option_template_name = "core/widgets/pill_checkbox_option.html"
