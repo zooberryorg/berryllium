b69d308e82beadb6c5d63e9dce2ea30abc3dcd82
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:42:23 -0700

    Add docstrings for NAVIGATION config

--- berryllium/mods/views/upload.py
+++ berryllium/mods/views/upload.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from ..forms import FileUploadForm
 
-# Create your views here.
-
+# Navigation configuration for upload form
+#     name: Current location title
+#     url: Reference Django url
+#     icon: Icon name. See: https://icons.getbootstrap.com/
+NAVIGATION = [
+    {'name': 'Basic Information', 'url': 'create_mods_step1', 'icon': 'info-circle'},
+    {'name': 'Upload Files', 'url': 'create_mods_step2', 'icon': 'upload'},
+    {'name': 'Organize Files', 'url': 'create_mods_step3', 'icon': 'folder'},
+    {'name': 'Review & Submit', 'url': 'create_mods_step4', 'icon': 'check-circle'},
+]
 
 def upload_mod(request):
     """
     Landing page for mod upload requests.
     """
-    return render(request, "mods/upload/base.html")
+    return render(request, "mods/upload/base.html", {
+        'form': FileUploadForm(),
+        'mod_navigation': NAVIGATION,
+    })
+
 
 
 def upload_step1(request):
