740d8e659b1fe93175aa3c325b7467423dd74186
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 14:51:35 -0700

    Reformat changes to new py files

--- berryllium/mods/urls.py
+++ berryllium/mods/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("mods/upload/", views.upload_mod, name="upload_mod")
-]
+urlpatterns = [path("mods/upload/", views.upload_mod, name="upload_mod")]
--- berryllium/mods/views.py
+++ berryllium/mods/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def upload_mod(request):
-    return render(request, 'mods/upload/base.html')
+    return render(request, "mods/upload/base.html")
