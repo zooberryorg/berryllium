c52011c563fed9ef3322bbd12f4037cdd0d2c784
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:46:06 -0700

    Port upload_step1 from old codebase and update references

--- berryllium/mods/views/upload.py
+++ berryllium/mods/views/upload.py
@@ -27,4 +27,36 @@ def upload_step1(request):
     """
     Step 1 of the upload form.
     """
-    return render(request, "mods/upload/step/1.html")
+    current_index = 1
+    progress_range = range(current_index)
+    remainder_range = range(current_index, len(NAVIGATION))
+    nav_item_count = len(NAVIGATION)
+    context = {
+        'form': MetadataForm(),
+        'filename': request.session.get('upload_original_name'),
+        'nav_item_count': nav_item_count,
+        'current_nav_index': 0,
+        'progress_range': progress_range,
+        'remainder_range': remainder_range,
+    }
+
+    if request.method == 'POST':
+        form = MetadataForm(request.POST)
+        if form.is_valid():
+            # Store metadata in session
+            request.session['upload_metadata'] = form.cleaned_data
+            
+            # Return NEXT state (step 2) on success
+            return redirect('create_mods_step2')
+        
+        # Invalid: return SAME state with errors
+        context['form'] = form
+        return render(request, 'mods/upload/step/1.html', context=context)
+    elif request.method == 'GET' and request.session.get('upload_metadata'):
+        # Send existing files for review at form initialization TODO: simply send a boolean
+        form = MetadataForm(initial=request.session['upload_metadata'])
+        context['form'] = form
+        return render(request, 'mods/upload/step/1.html', context=context)
+        
+    # GET: show fresh form
+    return render(request, 'mods/upload/step/1.html', context=context)
