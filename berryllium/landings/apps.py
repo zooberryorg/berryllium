f88fe4d697d702238b23493d4151873cc347449d
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 01:43:30 -0700

    Namespaced application name

--- berryllium/landings/apps.py
+++ berryllium/landings/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LandingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'landings'
+    name = 'berryllium.landings'
