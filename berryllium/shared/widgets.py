a87b96a4344fc3956a0587c2bbcb01d22559d4b8
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 15:58:36 -0700

    Fix faulty pill checkbox references in forms

--- berryllium/shared/widgets.py
+++ berryllium/shared/widgets.py
@@ -5,5 +5,5 @@ class PillCheckboxSelectMultiple(CheckboxSelectMultiple):
     """
     Custom checkbox design for Berryllium template.
     """
-    template_name = "core/widgets/pill_checkbox_select.html"
-    option_template_name = "core/widgets/pill_checkbox_option.html"
+    template_name = "shared/widgets/pill_checkbox_select.html"
+    option_template_name = "shared/widgets/pill_checkbox_option.html"
