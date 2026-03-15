e3a89f8eb9e348540f6a9c8f92ed776c2aa3ef08
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-03-15 14:07:20 -0700

    [Chore] Register mods/models.py to admin portal (#15)
    * Update docker-compose.yml
    
    * Register mod models to admin portal

--- berryllium/mods/admin.py
+++ berryllium/mods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Mod, Dependency, FileUpload, FileGroup, FileGroupMembership
 
 # Register your models here.
+admin.site.register(Mod)
+admin.site.register(Dependency)
+admin.site.register(FileUpload)
+admin.site.register(FileGroup)
+admin.site.register(FileGroupMembership)
