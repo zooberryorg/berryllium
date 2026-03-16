0fa8bb79d48e1436f3158dd3b4e7a781a7fd7ada
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-03-15 23:56:20 -0700

    [Task] Refactor styles and template partials related to step 2 of the upload form (#36)
    * Svg to ion icon in step 2
    
    * Add margin below uploaded files
    
    * Add margin below drop zone
    
    * Add warning icon to error
    
    * Decouple fileUploader script to js file
    
    * Add ref to meta
    
    * Fix incorrect static directory join path in settings.py
    
    * Decouple previous files to its own partial
    
    * Decouple dropzone as its own partial
    
    * Reformat all template files

---
+++ berryllium/mods/static/mods/js/upload_widget.js
@@ -0,0 +1,29 @@
+function fileUploader() {
+  return {
+    isDragging: false,
+    isUploading: false,
+    
+    handleFileSelect(event) {
+      const file = event.target.files[0];
+      if (file) {
+        console.log('File selected:', file.name, file.size);
+        // Click the hidden submit button to trigger htmx
+        this.$refs.submitBtn.click();
+      }
+    },
+    
+    handleDrop(event) {
+      this.isDragging = false;
+      const file = event.dataTransfer.files[0];
+      if (file) {
+        console.log('File dropped:', file.name, file.size);
+        // Set the file to the input
+        const dt = new DataTransfer();
+        dt.items.add(file);
+        this.$refs.fileInput.files = dt.files;
+        // Click the hidden submit button
+        this.$refs.submitBtn.click();
+      }
+    }
+  }
+}
