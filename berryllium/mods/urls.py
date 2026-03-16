b89811a25f55ee025ba26b6e509ad151d80b3e9c
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 17:32:52 -0700

    Additional missing upload_step3 references

--- berryllium/mods/urls.py
+++ berryllium/mods/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("mods/upload/", upload.upload_mod, name="upload_mod"),
     path("mods/upload/s1", upload.upload_step1, name="upload_step1"),
     path("mods/upload/s2", upload.upload_step2, name="upload_step2"),
+    path("mods/upload/s3", upload.upload_step3, name="upload_step3"),
     # Explore mods
     path("explore/mods/", explore.mods, name="explore_mods"),
 ]
