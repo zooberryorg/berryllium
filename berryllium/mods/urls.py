bae1ac2211aa6704632914d1c604b8bbfd3bd449
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-03-15 17:12:27 -0700

    [Chore] Port explore/mods templates and reformat files (#26)
    * Port shared/explore/base.html and reformat
    
    * Port mods/explore/base.html and reformat
    
    * Port mods/explore/filters.html and reformat
    
    * Port mods/explore/categories.html and reformat
    
    * Link in explore menu leads to 'explore_mods'
    
    * Url configuration for explore.mods
    
    * Create stubbed explore.mods view
    
    * Update docker-compose.yml

--- berryllium/mods/urls.py
+++ berryllium/mods/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import upload
+from .views import upload, explore
 
 urlpatterns = [
+    # Upload form
     path("mods/upload/", upload.upload_mod, name="upload_mod"),
     path("mods/upload/s1", upload.upload_step1, name="upload_step1"),
     path("mods/upload/s2", upload.upload_step2, name="upload_step2"),
+    # Explore mods
+    path("explore/mods/", explore.mods, name="explore_mods"),
 ]
