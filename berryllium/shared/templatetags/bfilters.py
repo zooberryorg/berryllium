b40749a3f34cdf24ecb38f494b8acceb8cf3ce12
Eric Galvan <lericgalvan@outlook.com>
2026-03-15 20:10:13 -0700

    Port bfilter template tags from old codebase

---
+++ berryllium/shared/templatetags/bfilters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(list_obj, index):
+    try:
+        return list_obj[index]
+    except (IndexError, ValueError, TypeError):
+        return None
