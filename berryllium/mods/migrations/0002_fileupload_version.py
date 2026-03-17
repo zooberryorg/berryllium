fffa157032c2f8e891dfdfb886e295e7edc1a3b7
Eric Galvan <lericgalvan@outlook.com>
2026-03-16 22:54:37 -0700

    Reformat files

--- berryllium/mods/migrations/0002_fileupload_version.py
+++ berryllium/mods/migrations/0002_fileupload_version.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('mods', '0001_initial'),
+        ("mods", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='fileupload',
-            name='version',
+            model_name="fileupload",
+            name="version",
             field=models.CharField(blank=True, max_length=100),
         ),
     ]
