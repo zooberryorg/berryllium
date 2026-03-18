778b44a63ba18fd222c18f85d2f0978390416520
Eric Galvan <lericgalvan@outlook.com>
2026-03-18 00:41:19 -0700

    Move NAVIGATION to settings.py

--- berryllium/mods/views/upload.py
+++ berryllium/mods/views/upload.py
@@ -9,17 +9,6 @@ from django.http import HttpResponse
 from ..forms import FileUploadForm, MetadataForm, FileDetailsForm, FileGroupForm
 from ..models import FileUpload, Mod, FileGroup
 
-# Navigation configuration for upload form
-#     name: Current location title
-#     url: Reference Django url
-#     icon: Icon name. See: https://icons.getbootstrap.com/
-NAVIGATION = [
-    {"name": "Basic Information", "url": "upload_step1", "icon": "info-circle"},
-    {"name": "Upload Files", "url": "upload_step2", "icon": "upload"},
-    {"name": "Organize Files", "url": "upload_step3", "icon": "folder"},
-    # {'name': 'Review & Submit', 'url': 'create_mods_step4', 'icon': 'check-circle'},
-]
-
 # ----------------------- Helper functions ----------------------
 
 
