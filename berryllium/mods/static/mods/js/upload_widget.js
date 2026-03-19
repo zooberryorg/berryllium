ab2f949cf9b78d11515f54e4aae196ef96db12e4
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-03-19 02:11:12 -0700

    [Feature] Reactive UX validation between URL and file upload fields in FileUploadForm (#74)
    * Add urlFieldEnabled and fileDropzoneEnabled flags to state
    
    * Handle flags when enabled/disabled
    
    * Change local flag decs from flag to this[flag] for mutability
    
    * Update passed values to strings
    
    * Remove unused ref parameter from getFileQueueLenght
    
    * hx-trigger input delay to 500ms from 100ms
    
    * Add docstring at top of previous_files.html indicating AlpineJS req
    
    * Check existing files on init
    
    * Disable dropzone if url len > 0 on init
    
    * Added preliminary help-text in case either components are disabled
    
    * show and hide component functions
    
    * helpText hidden by default
    
    * Remove django conditional messages from template
    
    * Add help messages via Alpine state
    
    * fileInputProcessor passed by object instead of by ordered input
    
    * Move urlfield event handler to alpine state function
    
    * showHelp is a getter; pass in object to fileInputProcessor
    
    * Add data attributes to file list for JS enumeration
    
    * Update getFileQueueLength to count data attributes; event listener to track changes
    
    * Add  x-ref for file queue
    
    * Return empty HttpResponse on file deletion; add hx-target to delete btn
    
    * Rework file queue state track as an event handler
    
    * File counter instead of active state
    
    * Fix error preventing help box from showing
    
    * Disable url field when no files
    
    * Add $nextTick to ensure final file is removed
    
    * Update help message colors

--- berryllium/mods/static/mods/js/upload_widget.js
+++ berryllium/mods/static/mods/js/upload_widget.js
@@ -1,33 +1,100 @@
-function fileInputProcessor() {
+function fileInputProcessor({hasExistingUrl = false} = {}) {
   // note: implicit global vars go here; attach to window object
 
   return {
     isDragging: false,
     isUploading: false,
+    urlFieldEnabled: true,
+    fileDropzoneEnabled: true,
     fileCount: 0,
+    hasExistingUrl: hasExistingUrl,
+
+    // guarantee form state on page load
+    init() {
+      // get initial file queue length 
+      this.fileCount = this.getFileQueueLength();
+
+      if (this.hasFiles) {
+        this.disableInput(this.$refs.urlBlock, 'urlFieldEnabled');
+      }
+      if (this.hasExistingUrl) {
+        this.disableInput(this.$refs.dropzoneBlock, 'fileDropzoneEnabled');
+      }
+    },
+
+    // preset help messages for help text component
+    get helpMessage() {
+      // when files exist and url field disabled
+      if (this.hasFiles) {
+        return `Files currently added to queue. If you wish to add an external URL instead, remove all files from the queue first.`;
+      }
+      // when url exists and file dropzone disabled
+      if (this.hasExistingUrl) {
+        return `URL field is currently populated. If you wish to add files instead, clear the URL field first.`;
+      }
+      return '';
+    },
+
+    // show help text if either url exists or files exist
+    get showHelp() {
+        if (this.hasFiles || this.hasExistingUrl) {
+          this.$refs.helpText.classList.remove('hidden');
+        } else {
+          this.$refs.helpText.classList.add('hidden');
+        }
+        return this.hasFiles || this.hasExistingUrl;
+    },
+
+    // check if files are in the queue
+    get hasFiles() {
+      return this.fileCount > 0;
+    },
+
+    // event handlers
+    onUrlFieldInput() {
+      if (this.getUrlFieldLength() > 0) {
+        this.disableInput(this.$refs.dropzoneBlock, 'fileDropzoneEnabled');
+        this.hasExistingUrl = true;
+      } else {
+        this.enableInput(this.$refs.dropzoneBlock, 'fileDropzoneEnabled');
+        this.hasExistingUrl = false;
+      }
+    },
 
     getUrlFieldLength() {
       if (!this.$refs.urlInput) return 0;
       return this.$refs.urlInput.value.length;
     },
 
-    getFileQueueLength(ref) {
-      if (!this.$refs.fileInput) return 0;
-      return this.$refs.fileInput.files.length;
+    // fires when a file is removed from queue
+    updateQueueState() {
+      this.fileCount--;
+      if (!this.hasFiles) {
+        this.$nextTick(() => {
+          this.$refs.fileQueue.remove();
+        });
+        this.enableInput(this.$refs.urlBlock, 'urlFieldEnabled');
+      }
     },
 
-    enableInput(ref) {
+    getFileQueueLength() {
+      const fileQueue = document.querySelectorAll('[data-file]');
+      return fileQueue.length;
+    },
+
+    enableInput(ref, flag) {
+      this[flag] = true;
       ref.classList.remove('pointer-events-none', `opacity-20`);
     },
 
-    disableInput(ref) {
+    disableInput(ref, flag) {
+      this[flag] = false;
       ref.classList.add('pointer-events-none', `opacity-20`);
     },
     
     handleFileSelect(event) {
       const file = event.target.files[0];
       if (file) {
-        console.log('File selected:', file.name, file.size);
         // Click the hidden submit button to trigger htmx
         this.$refs.submitBtn.click();
       }
@@ -37,7 +104,6 @@ function fileInputProcessor() {
       this.isDragging = false;
       const file = event.dataTransfer.files[0];
       if (file) {
-        console.log('File dropped:', file.name, file.size);
         // Set the file to the input
         const dt = new DataTransfer();
         dt.items.add(file);
