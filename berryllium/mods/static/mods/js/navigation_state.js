3085d19638cf3430c3e3bef19aa72def8ef64bd9
Eric Galvan <lericgalvan@outlook.com>
2026-03-20 01:27:56 -0700

    Refactor navigation_state.js

--- berryllium/mods/static/mods/js/navigation_state.js
+++ berryllium/mods/static/mods/js/navigation_state.js
@@ -1,23 +1,48 @@
 function navigationState({currentIndex = 1} = {}) {
     return {
         currentIndex: currentIndex,
+        linkClasses: [
+            'text-white', 
+            'bg-gradient-to-r', 
+            'from-gold-500/50', 
+            'to-gold-300/50', 
+            'border-gold-300/30'
+        ],
+        iconActiveClass: 'text-gold-300',
+        iconInactiveClass: 'text-gray-400',
+        linkActiveBgClass: 'bg-gold-400/20',
+        linkInactiveBgClass: 'bg-transparent',
         updateNavigation() {
+
         const navLinks = document.querySelectorAll('[x-ref^="navLink-"]');
             navLinks.forEach((link, idx) => {
                 if (idx == this.currentIndex ) {
-                    link.classList.add(
-                        'text-white', 
-                        'bg-gradient-to-r', 
-                        'from-gold-500/50', 
-                        'to-gold-300/50', 
-                        'border-gold-300/30');
+                    // update link bg and text color
+                    link.classList.add(...this.linkClasses);
+                    // update inner div icon bg
+                    const innerDiv = link.querySelector('div');
+                    if (innerDiv) {
+                        innerDiv.classList.remove(this.linkInactiveBgClass);
+                        innerDiv.classList.add(this.linkActiveBgClass);
+                        // update icon color
+                        const icon = innerDiv.querySelector('i');
+                        if (icon) {
+                            icon.classList.remove(this.iconInactiveClass);
+                            icon.classList.add(this.iconActiveClass);
+                        }
+                    }
                 } else {
-                    link.classList.remove(
-                        'text-white', 
-                        'bg-gradient-to-r', 
-                        'from-gold-500/50', 
-                        'to-gold-300/50', 
-                        'border-gold-300/30');
+                    link.classList.remove(...this.linkClasses);
+
+                    const innerDiv = link.querySelector('div');
+                    if (innerDiv) {
+                        innerDiv.classList.remove(this.linkActiveBgClass);
+                        const icon = innerDiv.querySelector('i');
+                        if (icon) {
+                            icon.classList.add(this.iconInactiveClass);
+                            icon.classList.remove(this.iconActiveClass);
+                        }
+                    }
                 }
         });
         }
