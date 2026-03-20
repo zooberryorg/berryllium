ce2a2d637dd156d808cb67c877b92180f4af8d5e
Eric Galvan <lericgalvan@outlook.com>
2026-03-20 00:36:27 -0700

    Moved prev navigationState function to static file

--- berryllium/mods/static/mods/js/navigation_state.js
+++ berryllium/mods/static/mods/js/navigation_state.js
@@ -1,4 +1,16 @@
-function navigationState() {
-  return {
-  }
+function navigationState({currentIndex = 1} = {}) {
+    return {
+        currentIndex: currentIndex,
+        updateNavigation() {
+        const navLinks = document.querySelectorAll('[x-ref^="navLink-"]');
+            navLinks.forEach((link, idx) => {
+            if (idx < this.currentIndex) {
+                link.classList.add('bg-gold-400/30', 'text-white', 'border-gold-300/30');
+            } else {
+                link.classList.remove('bg-gold-400/30', 'text-white', 'border-gold-300/30');
+                link.classList.add('text-gray-400', 'hover:bg-gradient-to-r', 'hover:from-gold-500/50', 'hover:to-gold-300/50');
+            }
+            });
+        }
+    }
 }
