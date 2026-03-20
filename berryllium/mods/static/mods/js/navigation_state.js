c4b8301b85540c528cf30b1114de19af266df733
Eric Galvan <lericgalvan@outlook.com>
2026-03-20 00:50:53 -0700

    Dynamic x-ref navLink in progress_bar.html

--- berryllium/mods/static/mods/js/navigation_state.js
+++ berryllium/mods/static/mods/js/navigation_state.js
@@ -4,11 +4,12 @@ function navigationState({currentIndex = 1} = {}) {
         updateNavigation() {
         const navLinks = document.querySelectorAll('[x-ref^="navLink-"]');
             navLinks.forEach((link, idx) => {
-            if (idx < this.currentIndex) {
-                link.classList.add('bg-gold-400/30', 'text-white', 'border-gold-300/30');
+            if (idx == this.currentIndex - 1) {
+                link.classList.add('bg-pine-300', 'text-pine-900');
+                link.classList.remove('bg-pine-600', 'text-pine-300');
             } else {
-                link.classList.remove('bg-gold-400/30', 'text-white', 'border-gold-300/30');
-                link.classList.add('text-gray-400', 'hover:bg-gradient-to-r', 'hover:from-gold-500/50', 'hover:to-gold-300/50');
+                link.classList.remove('bg-pine-300', 'text-pine-900');
+                link.classList.add('bg-pine-600', 'text-pine-300');
             }
             });
         }
