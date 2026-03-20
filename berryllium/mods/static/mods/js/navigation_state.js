cdddbb0c52ee9dd3a6e6aefe546355c854408a46
Eric Galvan <lericgalvan@outlook.com>
2026-03-20 01:14:33 -0700

    Reformat navigation_state.js

--- berryllium/mods/static/mods/js/navigation_state.js
+++ berryllium/mods/static/mods/js/navigation_state.js
@@ -4,12 +4,21 @@ function navigationState({currentIndex = 1} = {}) {
         updateNavigation() {
         const navLinks = document.querySelectorAll('[x-ref^="navLink-"]');
             navLinks.forEach((link, idx) => {
-                console.log(`Updating nav link ${idx + 1}: currentIndex=${this.currentIndex}: text=${link.textContent.trim()}`);
-            if (idx == this.currentIndex ) {
-                link.classList.add('text-white', 'bg-gradient-to-r', 'from-gold-500/50', 'to-gold-300/50', 'border-gold-300/30');
-            } else {
-                link.classList.remove('text-white', 'bg-gradient-to-r', 'from-gold-500/50', 'to-gold-300/50', 'border-gold-300/30');
-            }
+                if (idx == this.currentIndex ) {
+                    link.classList.add(
+                        'text-white', 
+                        'bg-gradient-to-r', 
+                        'from-gold-500/50', 
+                        'to-gold-300/50', 
+                        'border-gold-300/30');
+                } else {
+                    link.classList.remove(
+                        'text-white', 
+                        'bg-gradient-to-r', 
+                        'from-gold-500/50', 
+                        'to-gold-300/50', 
+                        'border-gold-300/30');
+                }
         });
         }
     }
