cb9c6e8e5e0a52b47b9b36e879fb36165bdbd246
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-03-22 02:51:41 -0700

    [Chore] Remove unused line in mods/upload/../group_actions.html (#143)
    * Remove stale code in group_actions.html
    
    * Reformat files

--- berryllium/shared/templatetags/bfilters.py
+++ berryllium/shared/templatetags/bfilters.py
@@ -6,16 +6,19 @@ register = template.Library()
 
 # --------------------------- filters
 
+
 @register.filter
 def get_item(list_obj, index):
     try:
         return list_obj[index]
     except (IndexError, ValueError, TypeError):
         return None
-    
+
+
 # --------------------------- template tags
+
 
 @register.simple_tag
 def gen_field(field, **kwargs):
-    extra = {k.replace('_', '-'): v for k, v in kwargs.items()}
+    extra = {k.replace("_", "-"): v for k, v in kwargs.items()}
     return mark_safe(field.as_widget(attrs=extra))
