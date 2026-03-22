d55e42c42c5ff2a76bd135a89f7098469a00474b
Eric Galvan <lericgalvan@outlook.com>
2026-03-22 01:09:16 -0700

    Template tag attempt to replace render_field

--- berryllium/shared/templatetags/bfilters.py
+++ berryllium/shared/templatetags/bfilters.py
@@ -1,7 +1,9 @@
 from django import template
+from widget_tweaks.templatetags.widget_tweaks import render_field
 
 register = template.Library()
 
+# --------------------------- filters
 
 @register.filter
 def get_item(list_obj, index):
@@ -9,3 +11,10 @@ def get_item(list_obj, index):
         return list_obj[index]
     except (IndexError, ValueError, TypeError):
         return None
+    
+# --------------------------- template tags
+
+@register.simple_tag
+def gen_field(field, **kwargs):
+    attrs = {k.replace('_', '-'): v for k, v in kwargs.items()}
+    return render_field(field, **attrs)
