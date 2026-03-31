a244726077cf476a956d23a2937c1db15f2ea5dc
Eric Galvan <lericgalvan@outlook.com>
2026-03-31 00:14:41 -0700

    Register markdownx to admin panel

--- berryllium/mods/admin.py
+++ berryllium/mods/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Mod, ModDependency, ModFile, ModFileGroup, ModImage
+from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
-admin.site.register(Mod)
+admin.site.register(Mod, MarkdownxModelAdmin)
 admin.site.register(ModDependency)
 admin.site.register(ModFile)
 admin.site.register(ModFileGroup)
