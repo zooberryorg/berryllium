faf2c9989812ab2e0c4f9f86d7c8d7f36780d361
Eric Galvan <lericgalvan@outlook.com>
2026-03-30 23:45:17 -0700

    Register mod image to admin panel

--- berryllium/mods/admin.py
+++ berryllium/mods/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Mod, ModDependency, ModFile, ModFileGroup
+from .models import Mod, ModDependency, ModFile, ModFileGroup, ModImage
 
 # Register your models here.
 admin.site.register(Mod)
 admin.site.register(ModDependency)
 admin.site.register(ModFile)
 admin.site.register(ModFileGroup)
+admin.site.register(ModImage)
