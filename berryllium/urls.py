739cca65b69e3975eecae94e4f27be582be66e9e
Eric Galvan <lericgalvan@outlook.com>
2026-03-31 00:12:07 -0700

    URL config for markdownx

--- berryllium/urls.py
+++ berryllium/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("berryllium.landings.urls")),
     path("", include("berryllium.mods.urls")),
+    path('markdownx/', include('markdownx.urls')),
 ]
 
 
