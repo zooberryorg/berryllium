62eba884053eb3f41b988161764b265c36b04146
Eric Galvan <lericgalvan@outlook.com>
2026-04-01 00:14:53 -0700

    Create initial Organization model

--- berryllium/organizations/models.py
+++ berryllium/organizations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    """
+    Represents an organization that can have multiple mods under it.
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    members = models.ManyToManyField("auth.User", related_name="organizations", through="OrganizationMembership")
+
+    def __str__(self):
+        return self.name
