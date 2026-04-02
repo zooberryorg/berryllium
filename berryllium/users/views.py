36c48149489e50270d8debdc49880894370d2b80
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 01:51:18 -0700

    Add forms to endpoints

--- berryllium/users/views.py
+++ berryllium/users/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.views.generic import CreateView, TemplateView, FormView, UpdateView
 
 from berryllium.users.models import Member
-
+from berryllium.users.forms import MemberRegistrationForm, MemberLoginForm
 
 class MemberRegistration(CreateView):
     model = Member
+    form_class = MemberRegistrationForm
     template_name = "users/register.html"
     success_url = "/"
 
     def form_valid(self, form):
         return super().form_valid(form)
     
+class MemberLogin(FormView):
+    form_class = MemberLoginForm
+    template_name = "users/login.html"
+    success_url = "/"
