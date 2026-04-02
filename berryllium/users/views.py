990bd4d02082a4f01829e547a45dc0b655f05dda
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 01:20:58 -0700

    Initial MemberRegistration view

--- berryllium/users/views.py
+++ berryllium/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView, FormView, UpdateView
 
-# Create your views here.
+from berryllium.users.models import Member
+
+
+class MemberRegistration(CreateView):
+    model = Member
+    template_name = "users/register.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
