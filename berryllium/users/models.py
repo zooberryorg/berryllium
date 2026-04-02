44f8a861545ab23cddff696dc47996b1baa8e361
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 00:39:52 -0700

    Define custom Member model

--- berryllium/users/models.py
+++ berryllium/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Member(AbstractUser):
+    """
+    Extends Django user. Pre-built fields:
+    - username
+    - password
+    - email
+    - first_name
+    - last_name
+    - is_staff
+    - is_active
+    - date_joined
+    - last_login
+    """
+    biography = models.TextField(blank=True, max_length=500)
+    is_archived = models.BooleanField(default=False)
