f0df466ac92b7cb5efa05d2ff63ff22dfd116c2b
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 01:52:20 -0700

    URL config from FBV to CBV

--- berryllium/users/urls.py
+++ berryllium/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from berryllium.users import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    path("register/", views.MemberRegistration.as_view(), name="register"),
+    path("login/", views.MemberLogin.as_view(), name="login"),
 ]
