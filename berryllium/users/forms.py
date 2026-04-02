e6c83345ec2b56a1d10a814c7e458b142f795381
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 02:34:58 -0700

    Field styles for login and registration forms

--- berryllium/users/forms.py
+++ berryllium/users/forms.py
@@ -1,5 +1,7 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelForm, TextInput
 
+from berryllium.mods.settings import DISABLE_SUBMIT_BUTTON_ATTRS
 from berryllium.users.models import Member
 
 
@@ -8,7 +10,80 @@ class MemberRegistrationForm(ModelForm):
         model = Member
         fields = ["username", "email", "password"]
 
+    username = forms.CharField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Enter username...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
+
+    email = forms.EmailField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Enter email...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Enter password...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
+
+    confirm_password = forms.CharField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Confirm password...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
+
+
 class MemberLoginForm(ModelForm):
     class Meta:
         model = Member
         fields = ["username", "password"]
+
+    username = forms.CharField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Enter username...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=TextInput(
+            attrs={
+                "placeholder": "Enter password...",
+                "class": "zb-input text-sm",
+                "autocomplete": "off",
+                **DISABLE_SUBMIT_BUTTON_ATTRS,
+            }
+        ),
+    )
