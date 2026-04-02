32443e2a4d0a1cd782cbaff402c4b6bdc2d8c5a7
Eric Galvan <lericgalvan@outlook.com>
2026-04-02 04:03:01 -0700

    Password match validation on reg

--- berryllium/users/forms.py
+++ berryllium/users/forms.py
@@ -58,6 +58,16 @@ class MemberRegistrationForm(ModelForm):
         ),
     )
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error("confirm_password", "Passwords do not match.")
+        
+        return cleaned_data
+
 
 class MemberLoginForm(ModelForm):
     class Meta:
